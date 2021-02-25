60a466dd1e110bab0866360d1e2155043863b29e
Salvador Arreola <l.salvador.arreola@gmail.com>
2021-02-25 14:14:54 -0600

    Revisar si existen los directorios de origen y destino, este ultimo si no existe se creará)

--- rename.py
+++ rename.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
-
 from os import listdir
 from os import rename
+import os
 import shutil
 import random 
 
 # Clase principal
 class AcomodoCanciones:
-    nuevoDir = "testNuevo/"
     dirMus = "Música Joinet/"
+    nuevoDir = "testNuevo/"
     separadorSpot = " ---- "
     maxCaracteres = 30
     lista = []
     cantidadSpots = 1
     enumerar = False
+
+    def verificarOrigen(self):
+        if os.path.exists(self.dirMus):
+            return True
+        else:
+            return False
+    def verificarDestino(self):
+        if os.path.exists(self.nuevoDir):
+            return True
+        else:
+            return False
+    def crearDestino(self):
+        if not os.path.exists(self.nuevoDir):
+            os.makedirs(self.nuevoDir)
+
 # variables globales
 separador = "----------------------------------------------------------------------"
 salir = False
@@ -30,8 +45,6 @@ def cambiarSeparador():
     playlist.separadorSpot = input("ingrese el nuevo separador: ")
 def cambiarCaracteresMax():
     playlist.maxCaracteres = input("ingrese el máximo de caracteres: ")
-def cambiarSeparador():
-    playlist.separadorSpot = input("ingrese el nuevo separador: ")
 def cambiarSpots():
     print(separador)
     playlist.cantidadSpots = int(input("ingrese la cantidad de spots: "))
@@ -54,35 +67,43 @@ def aleatorizar():
 def cambiarEnumerar():
     playlist.enumerar = not playlist.enumerar
 def generar():
-    print("generando lista en " + str(playlist.nuevoDir))
-    spotActual = 1
-    listaCan = listdir(playlist.dirMus)
-    n = 0
-    for cancion in listaCan:
-        enum = 0
-        nombreCancion = cancion[:-4]
-        caracteres = len(nombreCancion)
-        caracteresSobrantes = caracteres - playlist.maxCaracteres
+    if playlist.verificarOrigen():
+        if not playlist.verificarDestino():
+            playlist.crearDestino()
 
-        if caracteresSobrantes > 1:
-            nombreCancion = nombreCancion[:caracteres - caracteresSobrantes]
+        print("generando lista en " + str(playlist.nuevoDir))
+        spotActual = 1
+        listaCan = listdir(playlist.dirMus)
+        n = 0
 
-        nuevoNombre = (nombreCancion + ".mp3")
-        print(nuevoNombre)
-        print(playlist.nuevoDir+nombreCancion+playlist.separadorSpot+playlist.lista[spotActual-1] + " --- " +str(spotActual))
-        rename(playlist.dirMus+cancion, playlist.dirMus+nuevoNombre)
-        if playlist.enumerar:
-            shutil.copyfile(playlist.lista[spotActual-1], playlist.nuevoDir + str(f'{n:03}') + playlist.separadorSpot + playlist.lista[spotActual-1])
-            n = n + 1
-            shutil.copyfile(playlist.dirMus+nuevoNombre, playlist.nuevoDir + str(f'{n:03}') + ".mp3")
-            n = n + 1
-        else :
-            shutil.copyfile(playlist.lista[spotActual-1], playlist.nuevoDir+nombreCancion+playlist.separadorSpot+playlist.lista[spotActual-1])
-            shutil.copyfile(playlist.dirMus+nuevoNombre, playlist.nuevoDir+nuevoNombre)
-        if spotActual == len(playlist.lista):
-            spotActual = 1
-        else:
-            spotActual = spotActual + 1
+        for cancion in listaCan:
+            tamanioCadena = len(cancion)
+            formato = cancion[tamanioCadena - 4 :]
+            print(formato)
+            enum = 0
+            nombreCancion = cancion[:-4]
+            caracteres = len(nombreCancion)
+            caracteresSobrantes = caracteres - playlist.maxCaracteres
+
+            if caracteresSobrantes > 1:
+                nombreCancion = nombreCancion[:caracteres - caracteresSobrantes]
+
+            nuevoNombre = (nombreCancion + formato)
+            print(nuevoNombre)
+            print(playlist.nuevoDir + nombreCancion + playlist.separadorSpot + playlist.lista[spotActual-1] + " --- " +str(spotActual))
+            rename(playlist.dirMus+cancion, playlist.dirMus + nuevoNombre)
+            if playlist.enumerar:
+                shutil.copyfile(playlist.lista[spotActual-1], playlist.nuevoDir + str(f'{n:03}') + playlist.separadorSpot + playlist.lista[spotActual-1])
+                n = n + 1
+                shutil.copyfile(playlist.dirMus + nuevoNombre, playlist.nuevoDir + str(f'{n:03}') + formato)
+                n = n + 1
+            else :
+                shutil.copyfile(playlist.lista[spotActual-1], playlist.nuevoDir + nombreCancion + playlist.separadorSpot + playlist.lista[spotActual-1])
+                shutil.copyfile(playlist.dirMus + nuevoNombre, playlist.nuevoDir + nuevoNombre)
+            if spotActual == len(playlist.lista):
+                spotActual = 1
+            else:
+                spotActual = spotActual + 1
 
 # interface
 while not salir:
